85c562cbad083b3ad1ad8e92c096836b5f4bfb40
LeiBaron <LeiBaron@users.noreply.github.com>
2020-06-16 16:24:38 +0300

    newFiles

---
+++ story_content/user.js
@@ -0,0 +1,442 @@
+function ExecuteScript(strId)
+{
+  switch (strId)
+  {
+      case "5zZQvtFe6rM":
+        Script1();
+        break;
+      case "6IGYYK48PH6":
+        Script2();
+        break;
+      case "6bHsb8PDPOU":
+        Script3();
+        break;
+      case "6Dl0Bfb85l1":
+        Script4();
+        break;
+      case "6B8EaWTPn7N":
+        Script5();
+        break;
+      case "5Zk8s8mo5qC":
+        Script6();
+        break;
+      case "65pOMluVvsW":
+        Script7();
+        break;
+      case "5ulBIZUq37z":
+        Script8();
+        break;
+      case "6IT4jGbzQLF":
+        Script9();
+        break;
+      case "5ljYY7roOMA":
+        Script10();
+        break;
+      case "6UVQTHN5hr4":
+        Script11();
+        break;
+      case "5lWG7fhggVg":
+        Script12();
+        break;
+      case "6HNhBhAA0ca":
+        Script13();
+        break;
+      case "6arEiiEOVOE":
+        Script14();
+        break;
+      case "6N7cWJs9G9q":
+        Script15();
+        break;
+  }
+}
+
+function Script1()
+{
+  /*
+ This will insert a custom style for captions into a Storyline course.
+ T make it work, it has to be executed by a trigger, that is on the master slide and gets fired at timeline start.
+*/
+
+/*First test if the style is already there from another slide */
+var test = document.getElementById("MyStyle");
+
+/* Add the style only if it is not there to avoid cluttering the DOM */
+if(!test){
+ var body = document.querySelector("body");
+ /* create the element that will hold the style definition */
+ var style = document.createElement('style');
+ /* for older browsers it is necessary to give this type information */
+ style.type = 'text/css';
+ /* The new style element needs an ID so we can use it to test for its existance */
+ style.id = 'MyStyle';
+ /* The following is the style definition that will be applied */
+ style.appendChild(document.createTextNode(".caption p { background: rgba(255,255,255,0.7); color: black; text-decoration: none; text-shadow: none; font-weight: bold; }"));
+ /* finally append the element to the "body" of the document. */
+ body.appendChild(style);
+}
+}
+
+function Script2()
+{
+  /*
+ This will insert a custom style for captions into a Storyline course.
+ T make it work, it has to be executed by a trigger, that is on the master slide and gets fired at timeline start.
+*/
+
+/*First test if the style is already there from another slide */
+var test = document.getElementById("MyStyle");
+
+/* Add the style only if it is not there to avoid cluttering the DOM */
+if(!test){
+ var body = document.querySelector("body");
+ /* create the element that will hold the style definition */
+ var style = document.createElement('style');
+ /* for older browsers it is necessary to give this type information */
+ style.type = 'text/css';
+ /* The new style element needs an ID so we can use it to test for its existance */
+ style.id = 'MyStyle';
+ /* The following is the style definition that will be applied */
+ style.appendChild(document.createTextNode(".caption p { background: rgba(255,255,255,0.7); color: black; text-decoration: none; text-shadow: none; font-weight: bold; }"));
+ /* finally append the element to the "body" of the document. */
+ body.appendChild(style);
+}
+}
+
+function Script3()
+{
+  /*
+ This will insert a custom style for captions into a Storyline course.
+ T make it work, it has to be executed by a trigger, that is on the master slide and gets fired at timeline start.
+*/
+
+/*First test if the style is already there from another slide */
+var test = document.getElementById("MyStyle");
+
+/* Add the style only if it is not there to avoid cluttering the DOM */
+if(!test){
+ var body = document.querySelector("body");
+ /* create the element that will hold the style definition */
+ var style = document.createElement('style');
+ /* for older browsers it is necessary to give this type information */
+ style.type = 'text/css';
+ /* The new style element needs an ID so we can use it to test for its existance */
+ style.id = 'MyStyle';
+ /* The following is the style definition that will be applied */
+ style.appendChild(document.createTextNode(".caption p { background: rgba(255,255,255,0.7); color: black; text-decoration: none; text-shadow: none; font-weight: bold; }"));
+ /* finally append the element to the "body" of the document. */
+ body.appendChild(style);
+}
+}
+
+function Script4()
+{
+  /*
+ This will insert a custom style for captions into a Storyline course.
+ T make it work, it has to be executed by a trigger, that is on the master slide and gets fired at timeline start.
+*/
+
+/*First test if the style is already there from another slide */
+var test = document.getElementById("MyStyle");
+
+/* Add the style only if it is not there to avoid cluttering the DOM */
+if(!test){
+ var body = document.querySelector("body");
+ /* create the element that will hold the style definition */
+ var style = document.createElement('style');
+ /* for older browsers it is necessary to give this type information */
+ style.type = 'text/css';
+ /* The new style element needs an ID so we can use it to test for its existance */
+ style.id = 'MyStyle';
+ /* The following is the style definition that will be applied */
+ style.appendChild(document.createTextNode(".caption p { background: rgba(255,255,255,0.7); color: black; text-decoration: none; text-shadow: none; font-weight: bold; }"));
+ /* finally append the element to the "body" of the document. */
+ body.appendChild(style);
+}
+}
+
+function Script5()
+{
+  /*
+ This will insert a custom style for captions into a Storyline course.
+ T make it work, it has to be executed by a trigger, that is on the master slide and gets fired at timeline start.
+*/
+
+/*First test if the style is already there from another slide */
+var test = document.getElementById("MyStyle");
+
+/* Add the style only if it is not there to avoid cluttering the DOM */
+if(!test){
+ var body = document.querySelector("body");
+ /* create the element that will hold the style definition */
+ var style = document.createElement('style');
+ /* for older browsers it is necessary to give this type information */
+ style.type = 'text/css';
+ /* The new style element needs an ID so we can use it to test for its existance */
+ style.id = 'MyStyle';
+ /* The following is the style definition that will be applied */
+ style.appendChild(document.createTextNode(".caption p { background: rgba(255,255,255,0.7); color: black; text-decoration: none; text-shadow: none; font-weight: bold; }"));
+ /* finally append the element to the "body" of the document. */
+ body.appendChild(style);
+}
+}
+
+function Script6()
+{
+  /*
+ This will insert a custom style for captions into a Storyline course.
+ T make it work, it has to be executed by a trigger, that is on the master slide and gets fired at timeline start.
+*/
+
+/*First test if the style is already there from another slide */
+var test = document.getElementById("MyStyle");
+
+/* Add the style only if it is not there to avoid cluttering the DOM */
+if(!test){
+ var body = document.querySelector("body");
+ /* create the element that will hold the style definition */
+ var style = document.createElement('style');
+ /* for older browsers it is necessary to give this type information */
+ style.type = 'text/css';
+ /* The new style element needs an ID so we can use it to test for its existance */
+ style.id = 'MyStyle';
+ /* The following is the style definition that will be applied */
+ style.appendChild(document.createTextNode(".caption p { background: rgba(255,255,255,0.7); color: black; text-decoration: none; text-shadow: none; font-weight: bold; }"));
+ /* finally append the element to the "body" of the document. */
+ body.appendChild(style);
+}
+}
+
+function Script7()
+{
+  /*
+ This will insert a custom style for captions into a Storyline course.
+ T make it work, it has to be executed by a trigger, that is on the master slide and gets fired at timeline start.
+*/
+
+/*First test if the style is already there from another slide */
+var test = document.getElementById("MyStyle");
+
+/* Add the style only if it is not there to avoid cluttering the DOM */
+if(!test){
+ var body = document.querySelector("body");
+ /* create the element that will hold the style definition */
+ var style = document.createElement('style');
+ /* for older browsers it is necessary to give this type information */
+ style.type = 'text/css';
+ /* The new style element needs an ID so we can use it to test for its existance */
+ style.id = 'MyStyle';
+ /* The following is the style definition that will be applied */
+ style.appendChild(document.createTextNode(".caption p { background: rgba(255,255,255,0.7); color: black; text-decoration: none; text-shadow: none; font-weight: bold; }"));
+ /* finally append the element to the "body" of the document. */
+ body.appendChild(style);
+}
+}
+
+function Script8()
+{
+  /*
+ This will insert a custom style for captions into a Storyline course.
+ T make it work, it has to be executed by a trigger, that is on the master slide and gets fired at timeline start.
+*/
+
+/*First test if the style is already there from another slide */
+var test = document.getElementById("MyStyle");
+
+/* Add the style only if it is not there to avoid cluttering the DOM */
+if(!test){
+ var body = document.querySelector("body");
+ /* create the element that will hold the style definition */
+ var style = document.createElement('style');
+ /* for older browsers it is necessary to give this type information */
+ style.type = 'text/css';
+ /* The new style element needs an ID so we can use it to test for its existance */
+ style.id = 'MyStyle';
+ /* The following is the style definition that will be applied */
+ style.appendChild(document.createTextNode(".caption p { background: rgba(255,255,255,0.7); color: black; text-decoration: none; text-shadow: none; font-weight: bold; }"));
+ /* finally append the element to the "body" of the document. */
+ body.appendChild(style);
+}
+}
+
+function Script9()
+{
+  /*
+ This will insert a custom style for captions into a Storyline course.
+ T make it work, it has to be executed by a trigger, that is on the master slide and gets fired at timeline start.
+*/
+
+/*First test if the style is already there from another slide */
+var test = document.getElementById("MyStyle");
+
+/* Add the style only if it is not there to avoid cluttering the DOM */
+if(!test){
+ var body = document.querySelector("body");
+ /* create the element that will hold the style definition */
+ var style = document.createElement('style');
+ /* for older browsers it is necessary to give this type information */
+ style.type = 'text/css';
+ /* The new style element needs an ID so we can use it to test for its existance */
+ style.id = 'MyStyle';
+ /* The following is the style definition that will be applied */
+ style.appendChild(document.createTextNode(".caption p { background: rgba(255,255,255,0.7); color: black; text-decoration: none; text-shadow: none; font-weight: bold; }"));
+ /* finally append the element to the "body" of the document. */
+ body.appendChild(style);
+}
+}
+
+function Script10()
+{
+  /*
+ This will insert a custom style for captions into a Storyline course.
+ T make it work, it has to be executed by a trigger, that is on the master slide and gets fired at timeline start.
+*/
+
+/*First test if the style is already there from another slide */
+var test = document.getElementById("MyStyle");
+
+/* Add the style only if it is not there to avoid cluttering the DOM */
+if(!test){
+ var body = document.querySelector("body");
+ /* create the element that will hold the style definition */
+ var style = document.createElement('style');
+ /* for older browsers it is necessary to give this type information */
+ style.type = 'text/css';
+ /* The new style element needs an ID so we can use it to test for its existance */
+ style.id = 'MyStyle';
+ /* The following is the style definition that will be applied */
+ style.appendChild(document.createTextNode(".caption p { background: rgba(255,255,255,0.7); color: black; text-decoration: none; text-shadow: none; font-weight: bold; }"));
+ /* finally append the element to the "body" of the document. */
+ body.appendChild(style);
+}
+}
+
+function Script11()
+{
+  /*
+ This will insert a custom style for captions into a Storyline course.
+ T make it work, it has to be executed by a trigger, that is on the master slide and gets fired at timeline start.
+*/
+
+/*First test if the style is already there from another slide */
+var test = document.getElementById("MyStyle");
+
+/* Add the style only if it is not there to avoid cluttering the DOM */
+if(!test){
+ var body = document.querySelector("body");
+ /* create the element that will hold the style definition */
+ var style = document.createElement('style');
+ /* for older browsers it is necessary to give this type information */
+ style.type = 'text/css';
+ /* The new style element needs an ID so we can use it to test for its existance */
+ style.id = 'MyStyle';
+ /* The following is the style definition that will be applied */
+ style.appendChild(document.createTextNode(".caption p { background: rgba(255,255,255,0.7); color: black; text-decoration: none; text-shadow: none; font-weight: bold; }"));
+ /* finally append the element to the "body" of the document. */
+ body.appendChild(style);
+}
+}
+
+function Script12()
+{
+  /*
+ This will insert a custom style for captions into a Storyline course.
+ T make it work, it has to be executed by a trigger, that is on the master slide and gets fired at timeline start.
+*/
+
+/*First test if the style is already there from another slide */
+var test = document.getElementById("MyStyle");
+
+/* Add the style only if it is not there to avoid cluttering the DOM */
+if(!test){
+ var body = document.querySelector("body");
+ /* create the element that will hold the style definition */
+ var style = document.createElement('style');
+ /* for older browsers it is necessary to give this type information */
+ style.type = 'text/css';
+ /* The new style element needs an ID so we can use it to test for its existance */
+ style.id = 'MyStyle';
+ /* The following is the style definition that will be applied */
+ style.appendChild(document.createTextNode(".caption p { background: rgba(255,255,255,0.7); color: black; text-decoration: none; text-shadow: none; font-weight: bold; }"));
+ /* finally append the element to the "body" of the document. */
+ body.appendChild(style);
+}
+}
+
+function Script13()
+{
+  /*
+ This will insert a custom style for captions into a Storyline course.
+ T make it work, it has to be executed by a trigger, that is on the master slide and gets fired at timeline start.
+*/
+
+/*First test if the style is already there from another slide */
+var test = document.getElementById("MyStyle");
+
+/* Add the style only if it is not there to avoid cluttering the DOM */
+if(!test){
+ var body = document.querySelector("body");
+ /* create the element that will hold the style definition */
+ var style = document.createElement('style');
+ /* for older browsers it is necessary to give this type information */
+ style.type = 'text/css';
+ /* The new style element needs an ID so we can use it to test for its existance */
+ style.id = 'MyStyle';
+ /* The following is the style definition that will be applied */
+ style.appendChild(document.createTextNode(".caption p { background: rgba(255,255,255,0.7); color: black; text-decoration: none; text-shadow: none; font-weight: bold; }"));
+ /* finally append the element to the "body" of the document. */
+ body.appendChild(style);
+}
+}
+
+function Script14()
+{
+  /*
+ This will insert a custom style for captions into a Storyline course.
+ T make it work, it has to be executed by a trigger, that is on the master slide and gets fired at timeline start.
+*/
+
+/*First test if the style is already there from another slide */
+var test = document.getElementById("MyStyle");
+
+/* Add the style only if it is not there to avoid cluttering the DOM */
+if(!test){
+ var body = document.querySelector("body");
+ /* create the element that will hold the style definition */
+ var style = document.createElement('style');
+ /* for older browsers it is necessary to give this type information */
+ style.type = 'text/css';
+ /* The new style element needs an ID so we can use it to test for its existance */
+ style.id = 'MyStyle';
+ /* The following is the style definition that will be applied */
+ style.appendChild(document.createTextNode(".caption p { background: rgba(255,255,255,0.7); color: black; text-decoration: none; text-shadow: none; font-weight: bold; }"));
+ /* finally append the element to the "body" of the document. */
+ body.appendChild(style);
+}
+}
+
+function Script15()
+{
+  /*
+ This will insert a custom style for captions into a Storyline course.
+ T make it work, it has to be executed by a trigger, that is on the master slide and gets fired at timeline start.
+*/
+
+/*First test if the style is already there from another slide */
+var test = document.getElementById("MyStyle");
+
+/* Add the style only if it is not there to avoid cluttering the DOM */
+if(!test){
+ var body = document.querySelector("body");
+ /* create the element that will hold the style definition */
+ var style = document.createElement('style');
+ /* for older browsers it is necessary to give this type information */
+ style.type = 'text/css';
+ /* The new style element needs an ID so we can use it to test for its existance */
+ style.id = 'MyStyle';
+ /* The following is the style definition that will be applied */
+ style.appendChild(document.createTextNode(".caption p { background: rgba(255,255,255,0.7); color: black; text-decoration: none; text-shadow: none; font-weight: bold; }"));
+ /* finally append the element to the "body" of the document. */
+ body.appendChild(style);
+}
+}
+
